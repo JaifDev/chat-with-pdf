04d12f7f2110b8e723e157fa70ad19272ed12371
Jaif <jaifm5525@gmail.com>
2024-06-14 22:42:33 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,15 @@ const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
-    
+
     return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
